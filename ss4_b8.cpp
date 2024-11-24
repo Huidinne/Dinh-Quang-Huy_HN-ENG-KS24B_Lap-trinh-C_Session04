7d3f0fc9e9cd936acb6d2b75b5a9027ee7ce07fc
Huidinne <quanghuy432006@gmail.com>
2024-11-24 10:24:56 +0700

    Add files via upload

---
+++ ss4_b8.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float a, b, c;
+    printf("Nhap 3 canh cua tam giac: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if ((a + b > c) && (a + c > b) && (b + c > a)){
+        printf("La 3 canh tam giac");
+    }else{
+        printf("Khong phai 3 canh tam giac");
+    }
+}
+
